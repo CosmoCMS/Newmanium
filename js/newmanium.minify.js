d831bd07c66d447b2eeba47db51340e5202705f4
James Dunn <JDDunn9@Gmail.com>
2014-10-27 15:47:14 -0400

    changes

--- js/newmanium.minify.js
+++ js/newmanium.minify.js
@@ -4,16 +4,28 @@ angular.module('newmanium', [])
     return {
         templateUrl: 'themes/Newmanium/partials/three-features.html',
         link: function(scope, elm, attrs){
+            elm.after('<br />');
             elm.removeAttr('three-features');
         }
     };
 }])
 
-.directive('featureGallery', [function(){
+.directive('featureGallery', ['Page', function(Page){
     return {
         templateUrl: 'themes/Newmanium/partials/feature-gallery.html',
         link: function(scope, elm, attrs){
-            elm.removeAttr('feature-gallery');
+            // Initialize variables
+            scope.featureGallery = {};
+            if(Page.extras['featuresGallery'])
+                scope.features = Page.extras['featuresGallery'];
+            else
+                scope.features = [];
+            
+            // Add a new feature window
+            scope.addFeature = function(){
+                scope.features.push({button: 'Button'});
+                Page.extras['featuresGallery'] = scope.features;
+            };
         }
     };
 }]);
